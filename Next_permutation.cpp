9349837a53d926c0e61c21e63c54d9f64f92564e
AyushHowale <118345207+AyushHowale@users.noreply.github.com>
2025-04-06 21:13:13 +0530

    Add files via upload

---
+++ Next_permutation.cpp
@@ -0,0 +1,10 @@
+
+// User function Template for C++
+
+class Solution {
+    public:
+      void nextPermutation(vector<int>& arr) {
+          // code here
+          next_permutation(arr.begin(),arr.end());
+      }
+  };
